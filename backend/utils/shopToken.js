850d9bad58720820b38412eb49bfe1c8b87e6475
rehmanmusharaf <muhammadrehmanmusharafali@gmail.com>
2024-08-13 11:56:57 -0400

    sendtoken Configuration change

--- backend/utils/shopToken.js
+++ backend/utils/shopToken.js
@@ -3,9 +3,15 @@ const sendToken = async (seller, statuscode, res) => {
     console.log("Send Token function run!");
     const token = await seller.getJwtToken();
     console.log("token is  : ", token);
+    // const option = {
+    //   expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
+    //   httpOnly: true,
+    // };
     const option = {
       expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
       httpOnly: true,
+      secure: process.env.NODE_ENV === "production", // Set secure flag to true in production
+      sameSite: "None", // Necessary for cross-site requests
     };
     res
       .status(statuscode)

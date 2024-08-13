b245a5b92d5b9407780441e4bba03134dc03fb33
rehmanmusharaf <muhammadrehmanmusharafali@gmail.com>
2024-08-13 08:47:20 -0400

    server url added

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,2 +1,3 @@
-export const server = "http://localhost:8000/";
+// export const server = "http://localhost:8000/";
+export const server = "https://e-shop-theta-sooty.vercel.app/";
 export const ENDPOINT = "http://localhost:4000/";

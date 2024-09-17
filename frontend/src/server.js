ddea1c7e6ea4c2d75f909a545357b5983e242b07
rehmanmusharaf <muhammadrehmanmusharafali@gmail.com>
2024-09-17 14:25:44 -0400

    send token function change

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,5 +1,7 @@
 // export const server = "http://localhost:8000/";
-export const server = "https://e-shop-mbrz.onrender.com/";
-// export const server = "http://localhost:8000/";
+// export const server = "https://e-shop-theta-sooty.vercel.app/";
+// export const server = "https://e-shop-mbrz.onrender.com/";
+
+export const server = "http://localhost:8000/";
 
 export const ENDPOINT = "http://localhost:4000/";

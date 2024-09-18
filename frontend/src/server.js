6466df9330bc2f15afc7f489ab465308484ed038
rehmanmusharaf <muhammadrehmanmusharafali@gmail.com>
2024-09-17 23:42:19 -0400

    add set cookies in cors

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -3,5 +3,5 @@
 // export const server = "https://e-shop-backend-ttx4.onrender.com/";
 
 export const server = "http://localhost:8000/";
-
 export const ENDPOINT = "http://localhost:4000/";
+// frontenddomain=https://e-shopingstore.netlify.app/
